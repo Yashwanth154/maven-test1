b9de3a45ba5b62a6460eb95a23fe9e4d7832ea8e
Yashwanth154 <122393666+Yashwanth154@users.noreply.github.com>
2024-10-08 17:51:07 +0530

    Revert "SaiYash"

--- src/main/java/com/example/javamavenjunithelloworld/Utility.py
+++
@@ -1,9 +0,0 @@
-b=int(input("Enter the value of b:"))
-m=int(input("Enter the value of m:"))
-w=int(input("Enter the value of w:"))
-if b>m and b>w:
-    print("b is greater than m,w")
-elif m>b and m>w:
-    print("m is greater than b,w")
-else:
-    print("w is greater than b,m")
